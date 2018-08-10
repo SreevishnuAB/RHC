4233f7b1e2c1e0800d6cdc4301cb3ba67e628679
SreevishnuAB <sreeab28@gmail.com>
2018-08-11 00:05:27 +0530

    Implementing gallery

--- Pages/BRSPage.js
+++ Pages/BRSPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, TouchableOpacity , Image , Dimensions} from 'react-native';
+import PhotoGrid from 'react-native-image-grid';
 import styles from '../CSS/css';
 
 export default class BRSPage extends React.Component{
@@ -7,8 +8,38 @@ export default class BRSPage extends React.Component{
     title: 'Base Retina Score',
   };
   
-  render(){
+  constructor(props) {
+    super(props);
+    this.state = { items: [] };
+  }
+
+  componentDidMount() {
+    let items = Array.apply(null, Array(100)).map((v, i) => {
+      return {id: i, src: 'https://res.cloudinary.com/praveenpi/image/upload/v1524920749/'+(i+1)+'.jpg' }
+    });
+    this.setState({ items });
+  }
+
+  _handlePress = () => {
+    this.props.navigation.navigate('HBA1C');
+  }
+  renderItem(item, itemSize) {
     return(
+      <TouchableOpacity
+        key = { item.id }
+        style = {{ width: itemSize, height: itemSize}}>
+        <Image
+          onPress = {this._handlePress}
+          resizeMode = "cover"
+          style = {{ flex: 1 }}
+          source = {{ uri: item.src }}
+        />
+      </TouchableOpacity>
+    )
+  }
+
+  render(){
+/*    return(
       <View
         style={styles.container}>
         <Text style={styles.text}>Base Retina Score *TODO* </Text>
@@ -17,6 +48,15 @@ export default class BRSPage extends React.Component{
           title="Next" 
           onPress={() => this.props.navigation.navigate('HBA1C')} />
       </View>
+    );*/
+    return(
+      <PhotoGrid
+        data = { this.state.items }
+        itemsPerRow = { 3 }
+        itemMargin = { 1 }
+        itemPaddingHorizontal={1}
+        renderItem = { this.renderItem }
+      />
     );
   }
 }
--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 3. [React-Native-image-pan-zoom](https://www.npmjs.com/package/react-native-image-pan-zoom)
 4. [MobX](https://mobx.js.org/) (v4.0.0)
 5. [MobX React](https://github.com/mobxjs/mobx-react)
+6. [ES6 Symbols]
+7. [React-Native-Image-Grid](https://www.npmjs.com/package/react-native-image-grid)
